6dc3bf4dc556e6e25c6041b04677c5d74b85c44a
reneraesp <rdejramirez@gmail.com>
2025-03-05 23:47:57 -0500

    deploy yml

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/tu-repositorio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
 })
